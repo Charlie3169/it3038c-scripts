694cf64e7381505cf5922253531f4e95afeb8ec4
Charlie3169 <shoelesscharlie@gmail.com>
2022-09-15 16:30:43 -0400

    powershell stuff

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,10 @@
+﻿function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+
+$IP = getIP
+$USER = whoami
+$DATE = Get-Date
+
+$BODY = ("This machine's IP is {0}. User is {1}. Hostname is {2}. PowerShell Version {3}. Today's Date is {4}" -f $IP, $USER, $HOST.Name, $HOST.Version, $DATE)
+$BODY |  Out-File C:\it3038c-scripts\powershell\systemInfo.txt
