0c4ac4c4b1648906a37f23ac2b3ef82551aac85b
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2022-11-13 21:21:31 -0500

    lab10

--- labs/Lab10API.js
+++ labs/Lab10API.js
@@ -60,31 +60,3 @@ app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-
-
-
-/* 
-
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.end("Test");    
-        
-      } else if (req.url === "/api") {        
-        
-        res.writeHead(200, {"Content-Type": "text/json"});
-        res.end(JSON.stringify(data));  
-
-      } else {
-
-        res.writeHead(404, {"Content-Type": "text/plain"});
-        res.end("Data not found");       
-
-      }    
-});
-
-server.listen(3000);
-console.log("Server listening on port 3000");
-
-*/
