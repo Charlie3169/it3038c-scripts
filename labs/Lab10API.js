1992c8e2a6e11e4e69e0551c7def9ce3c12b25d1
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2022-11-13 21:04:39 -0500

    heroku2

--- labs/Lab10API.js
+++ labs/Lab10API.js
@@ -1,44 +1,80 @@
 const http = require("http");
+const data = require("./widgets.json");
 const os = require("os");
 const ip = require('ip');
-const data = require("./widgets.json");
+
+const config = require('config')
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+    response.send
+    (
+        '<b>Hello World! My name is:<em>' + process.env.MYNAME + '</em> <br /> My Node Environment is:<em>' 
+        + config.util.getEnv('NODE_ENV') + '</em></b>'
+        + '<br/><br/>' +'<b><a href="/sysinfo">Get system info!</a></b>'
+    )
+})
+
+
+
+app.get('/api', (request, response) => {
+    response.send(JSON.stringify(data))    
+})
+
+app.get('/sysinfo', (request, response) => {   
+
+    myHostName=os.hostname();
+    serverUptime = os.uptime();
+    reducedDays = Math.floor(serverUptime / 86400);
+    reducedHours = Math.floor(serverUptime / 3600) - reducedDays * 24;
+    reducedMinutes = Math.floor(serverUptime / 60) - reducedDays * 1440 - reducedHours * 60;
+    reducedSeconds = serverUptime - reducedDays * 86400 - reducedHours * 3600 - reducedMinutes * 60;
+    totalMemory = (os.totalmem() / (1024**2)).toFixed(3);
+    freeMemory = (os.freemem() / (1024**2)).toFixed(3);
+    numCPUs = os.cpus().length;
+      
+    html=`    
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Node JS Response</title>
+      </head>
+      <body>
+        <p>Hostname: ${myHostName}</p>
+        <p>IP: ${ip.address()}</p>
+        <p>Server Uptime: Days: ${reducedDays}, Hours: ${reducedHours}, Minutes: ${reducedMinutes}, Seconds: ${reducedSeconds}</p>
+        <p>Total Memory: ${totalMemory} MB</p>
+        <p>Free Memory: ${freeMemory} MB</p>
+        <p>Number of CPUs: ${numCPUs}</p>
+      </body>
+    </html>`
+    
+    response.send(html);      
+})
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+
+
+
+/* 
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
 
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end("Test");    
+        
+      } else if (req.url === "/api") {        
+        
         res.writeHead(200, {"Content-Type": "text/json"});
-        res.end(JSON.stringify(data));        
-
-      } else if (req.url === "/api") {
-        res.writeHead(200, {"Content-Type": "text/html"});
-
-        myHostName=os.hostname();
-        serverUptime = os.uptime();        
-        reducedDays = Math.floor(serverUptime / 86400);
-        reducedHours = Math.floor(serverUptime / 3600) - reducedDays * 24;
-        reducedMinutes = Math.floor(serverUptime / 60) - reducedDays * 1440 - reducedHours * 60;
-        reducedSeconds = serverUptime - reducedDays * 86400 - reducedHours * 3600 - reducedMinutes * 60;
-        totalMemory = (os.totalmem() / (1024**2)).toFixed(3);
-        freeMemory = (os.freemem() / (1024**2)).toFixed(3);
-        numCPUs = os.cpus().length;
-        
-        html=`    
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Node JS Response</title>
-        </head>
-        <body>
-            <p>Hostname: ${myHostName}</p>
-            <p>IP: ${ip.address()}</p>
-            <p>Server Uptime: Days: ${reducedDays}, Hours: ${reducedHours}, Minutes: ${reducedMinutes}, Seconds: ${reducedSeconds}</p>
-            <p>Total Memory: ${totalMemory} MB</p>
-            <p>Free Memory: ${freeMemory} MB</p>
-            <p>Number of CPUs: ${numCPUs}</p>
-        </body>
-        </html>`
-        
-        res.end(html);  
+        res.end(JSON.stringify(data));  
 
       } else {
 
@@ -50,3 +86,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000);
 console.log("Server listening on port 3000");
+
+*/
